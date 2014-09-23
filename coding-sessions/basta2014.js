044c3bcc1a0ed879934ca0008e99aa158a5bce8e
olli <oliver.zeigermann@gmail.com>
2014-09-23 15:39:53 +0200

    Komplett mit commonjs Modulen

--- coding-sessions/basta2014.js
+++ coding-sessions/basta2014.js
@@ -1,30 +1,14 @@
-/*
- Content:
- * var
- * object
- * 'use strict'
- * console.log
- * environments: node und chrome
- * array
- * for
- * function
- * foreach
- * class pattern
- * iife
- * module pattern
- * CommonJS
- * TypeScript
- */
-// Cmd-K to commit changes
 'use strict';
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
-}
-Person.prototype.print = function () {
-    console.log("Name: " + this.name);
-    console.log("Age: " + this.age);
-};
-
+var Person = (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person.prototype.print = function () {
+        console.log("Name: " + this.name);
+        console.log("Age: " + this.age);
+    };
+    return Person;
+})();
 var olli = new Person("Olli", 43);
-exports.person = olli;
+//# sourceMappingURL=basta2014.js.map
