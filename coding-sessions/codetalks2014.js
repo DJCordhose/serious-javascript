6eaaf2ed206c0606395cd950bb43ebb64a0a2439
olli <oliver.zeigermann@gmail.com>
2014-09-23 18:33:46 +0200

    code.talks version of js intro

---
+++ coding-sessions/codetalks2014.js
@@ -0,0 +1,23 @@
+/*
+ Content:
+ * 'use strict'
+ * var
+ * object
+ * console.log
+ * environments: node und chrome
+ * array
+ * for
+ * function
+ * class pattern (keine Vererbung)
+ * iife
+ * module pattern
+ */
+// Cmd-K to commit changes
+'use strict';
+
+var person = {
+    name: "Oma",
+    age: 88
+};
+
+console.log(person);
