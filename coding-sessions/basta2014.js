a4c29819c75c836a7c0efd4a810d597d2ac661a5
olli <oliver.zeigermann@gmail.com>
2014-09-23 15:10:39 +0200

    Functions

--- coding-sessions/basta2014.js
+++ coding-sessions/basta2014.js
@@ -18,17 +18,20 @@
 // Cmd-K to commit changes
 'use strict';
 
+var print = function (person) {
+    console.log("Name: " + person.name);
+    console.log("Age: " + person.age);
+};
+
 var olli = {
     name: "Olli",
-    age: 43
+    age: 68
 };
 
 var array = [olli, {name: "Oma", age: 88}];
-for (var i in array) {
-    console.log(array[i]);
-}
 
-function print(person) {
-    console.log("Name: " + person.name);
-    console.log("Age: " + person.age);
-}
+var hasSeniorAge = function (person) {
+    return person.age > 67;
+};
+array.filter(hasSeniorAge).forEach(print);
+
