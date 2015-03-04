25a3c7096ae05e513eaff92f4b408cba00e5bea5
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-03-04 09:25:47 +0100

    finished framework talk

--- framework-comparison/backbone/hello.js
+++ framework-comparison/backbone/hello.js
@@ -1,4 +1,3 @@
-// alternative is to have more than one view and only re-render output view
 var Model = Backbone.Model.extend({
     defaults: {
         greeting: 'Hello'
@@ -6,38 +5,30 @@ var Model = Backbone.Model.extend({
 });
 
 var HelloView = Marionette.ItemView.extend({
-    initialize: function () {
-        this.model.on('change', this.updateGreeting, this);
-    },
     ui: {
         input: '#in',
         output: '#out'
     },
 
     events: {
-        'keyup @ui.input': 'onInputKeypress',
         'click #btn': 'onBtnClick'
     },
 
-    onInputKeypress: function () {
-        var greeting = this.ui.input.val().trim();
-        this.model.set('greeting', greeting);
+    bindings: {
+        '#in': 'greeting',
+        '#out': 'greeting'
     },
 
-    updateGreeting: function() {
-        this.ui.input.val(this.model.get('greeting'));
-        this.ui.output.html(this.model.get('greeting'));
-    },
-
-    onBtnClick: function() {
+    onBtnClick: function () {
         this.model.set('greeting', '');
-        this.render();
-    },
-    onRender: function() {
         this.ui.input.focus();
     },
 
-    template: _.template('<input id="in" value="<%-greeting%>"><p><span id="out"><%-greeting%></span>, World</p><input id="btn" type="button" value="Clear">')
+    onRender: function () {
+        this.stickit();
+    },
+
+    template: _.template('<input id="in"><p><span id="out"></span>, World</p><input id="btn" type="button" value="Clear">')
 });
 
 (new HelloView({
