60d26fb3e8a787059ee692251a84350576baf545
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-03-14 11:36:03 +0100

    preparation for live coding jax 2015

--- coding-sessions/jax2015/jax2015.js
+++ coding-sessions/jax2015/jax2015.js
@@ -17,3 +17,26 @@
  * let, const
  * class, extends
  */
+'use strict';
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    getName() {
+        return this.name;
+    }
+}
+class Programmer extends Person {
+    constructor(name, language) {
+        super(name);
+        this.language = language;
+    }
+    code() {
+        return this.getName() + " codes in " + this.language;
+    }
+}
+const programmer = new Programmer('Erna', 'JavaScript');
+console.log(programmer.code());
+console.log(programmer instanceof Programmer);
+console.log(programmer instanceof Person);
