ca5070b2862d0aec3ff921abc542185f57b8d068
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-04-19 15:33:30 +0200

    final preparation workshop for JAX 2015

---
+++ coding-sessions/jax2015/index.js
@@ -0,0 +1,23 @@
+/*
+ * /usr/local/bin/iojs --use_strict --harmony --es_staging --harmony_modules --harmony_arrow_functions --harmony_classes --harmony_object_literals jax2015.js
+ *
+ * Content:
+ *
+ * ES5 part
+ * 'use strict'
+ * var
+ * object
+ * console.log
+ * environments: node und chrome
+ * array
+ * for
+ * function
+ * iife
+ * class pattern (keine Vererbung)
+ *
+ * ES6 part
+ * let, const
+ * class, extends
+ *
+ */
+'use strict';
